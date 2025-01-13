536152244e229f9ba043f13341d6d738e68e8867
Frezeons <mahmutcan_gungor@hotmail.com>
2025-01-13 19:51:16 +0300

    deleted unnecessary implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -131,7 +131,4 @@ dependencies {
     implementation("io.coil-kt.coil3:coil-compose:3.0.4")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
 
-    implementation ("com.google.accompanist:accompanist-pager:0.32.0")
-    implementation ("com.google.accompanist:accompanist-pager-indicators:0.32.0")
-
 }
